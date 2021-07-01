a67ebfc71949518c956e6e4c3efe0963d9f2ea28
Vitor Silva <vitorabsilva10@gmail.com>
2021-07-01 20:20:38 -0300

    First Commit: Starting Header Component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/Header";
+import "./styles/global.scss";
+
+export function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from"./styles.module.scss";
+
+export function Header() {
+    return(
+        <div className={styles.container}>
+            <header>
+                <div className={styles.topHeader}>
+                    <h1>Codeland</h1>
+                    <h1>Blog</h1>
+                </div>  
+                <input type="text" placeholder="Pesquisar post" className={styles.input} />
+            </header>
+        </div>
+    );
+}
